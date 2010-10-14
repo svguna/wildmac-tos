37c5e83f77d9ee0b7817c94a247fa63c9a0e8be3
Stefan Guna <guna@disi.unitn.it>
2010-10-14 18:25:44 +0200

    added bulk experiment class

---
+++ apps/NeighborDetection/java-src/it/unitn/wildmac/BulkExperiment.java
@@ -0,0 +1,147 @@
+/**
+ * 
+ */
+package it.unitn.wildmac;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * A one shot experiment.
+ * 
+ * @author Stefan Guna
+ * 
+ */
+public class BulkExperiment extends TimerTask implements ReportConsumer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File logProperties = new File("log.config");
+		if (!logProperties.exists())
+			BasicConfigurator.configure();
+		else
+			PropertyConfigurator.configure(logProperties.getPath());
+
+		long period, duration, delay;
+		int beacon, samples;
+		boolean countFromMsg = false, randomDelay = false;
+		int nodes;
+
+		try {
+			nodes = new Integer(args[0]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Invalid number of nodes specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			period = new Integer(args[1]);
+		} catch (Exception e) {
+			System.err.println("Invalid protocol period specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			beacon = new Integer(args[2]);
+		} catch (Exception e) {
+			System.err.println("Invalid beacon duration specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			samples = new Integer(args[3]);
+		} catch (Exception e) {
+			System.err.println("Invalid number of samples specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			duration = new Integer(args[4]);
+		} catch (Exception e) {
+			System.err.println("Invalid experiment duration specified.");
+			printSyntax();
+			return;
+		}
+
+		try {
+			delay = new Integer(args[5]);
+		} catch (Exception e) {
+			System.err.println("Invalid delay specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			if (args[6].equals("on"))
+				countFromMsg = true;
+		} catch (Exception e) {
+			System.err.println("Invalid delay specified.");
+			printSyntax();
+			return;
+		}
+		try {
+			if (args[7].equals("on"))
+				randomDelay = true;
+		} catch (Exception e) {
+			System.err.println("Invalid randomicity specified.");
+			printSyntax();
+			return;
+		}
+
+		WSNGateway gateways[] = new WSNGateway[nodes];
+		BulkExperiment experiment = new BulkExperiment();
+
+		for (int i = 0; i < nodes; i++) {
+			String source = "serial@/dev/ttyUSB" + i + ":tmote";
+			gateways[i] = WSNGateway.getGateway(source);
+			gateways[i].registerConsumer(experiment);
+		}
+
+		try {
+			gateways[0].startExperiment(period, beacon, samples, duration,
+					delay, countFromMsg, randomDelay);
+		} catch (IOException e) {
+			System.err.println("Unable to communicate with the mote");
+			System.err.println(e.getMessage());
+			System.exit(-1);
+		}
+
+		Timer timeoutTimer = new Timer();
+		timeoutTimer.schedule(experiment, duration + delay);
+	}
+
+	private static void printSyntax() {
+		System.out.println("Syntax:");
+		System.out
+				.println("\t java "
+						+ BulkExperiment.class.getName()
+						+ " NODES PERIOD BEACON SAMPLES DURATION DELAY CNT_MSG RND_DELAY");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see it.unitn.wildmac.ReportConsumer#neighborDiscovered(int, int, long)
+	 */
+	public void neighborDiscovered(int nodeId, int neighbor, long timestamp) {
+		System.out.println(nodeId + " discovered " + neighbor + " in "
+				+ timestamp + " ms.");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.TimerTask#run()
+	 */
+	@Override
+	public void run() {
+		System.out.println("Experiment done.");
+		System.exit(0);
+	}
+}
