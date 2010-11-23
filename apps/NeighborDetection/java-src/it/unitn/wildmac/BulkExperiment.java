b70839fec02f502a0fbde125898e5212da6b0837
Stefan Guna <guna@disi.unitn.it>
2010-11-23 15:39:45 +0100

    final bulk experiments before the paper

--- apps/NeighborDetection/java-src/it/unitn/wildmac/BulkExperiment.java
+++ apps/NeighborDetection/java-src/it/unitn/wildmac/BulkExperiment.java
@@ -3,16 +3,35 @@
  */
 package it.unitn.wildmac;
 
+import java.awt.Toolkit;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.security.GeneralSecurityException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.TreeSet;
 
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
+
+import com.sun.mail.util.MailSSLSocketFactory;
 
 /**
  * A one shot experiment.
@@ -34,29 +53,42 @@ public class BulkExperiment implements ReportConsumer {
 			System.out.println("Experiment " + experimentCnt + " done.");
 
 			experimentCnt++;
-			if (experimentCnt == experimentMax)
+			if (experimentCnt == experimentMax) {
+				try {
+					sendAlert("Set of experiments finished", -1);
+				} catch (Exception e1) {
+					System.out.println("Unable to send notification");
+					System.out.println(e1.getMessage());
+				}
 				System.exit(0);
+			}
 			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
+			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 
-			try {
-				for (int i = 0; i < nodes; i++)
-					gateways[i].startExperiment(period, beacon, samples,
-							duration, rand.nextInt((int) period), countFromMsg,
-							false, true);
-			} catch (IOException e) {
-				System.err.println("Unable to communicate with the mote");
-				System.err.println(e.getMessage());
-			}
-
-			Timer timeoutTimer = new Timer();
-			timeoutTimer.schedule(new ExperimentStop(), duration + period * 2);
+			runNextExperiment();
 		}
 	}
+
+	private static class SMTPAuthenticator extends javax.mail.Authenticator {
+		private Properties props;
+
+		public SMTPAuthenticator(Properties props) {
+			this.props = props;
+		}
+
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(props
+					.getProperty("mail.smtp.user"), props
+					.getProperty("mail.smtp.password"));
+
+		}
+	}
+
+	private static final int ALERT_THRESHOLD = 5;
 
 	private static int beacon;
 
@@ -66,16 +98,21 @@ public class BulkExperiment implements ReportConsumer {
 	private static BulkExperiment experiment;
 	private static int experimentCnt = 0;
 	private static int experimentMax;
+	private static final int FIXED_DURATION = 2000;
+
 	private static WSNGateway gateways[];
 	private static int nodes;
 	private static long period;
+	private static int problemsInRow = 0;
 	private static Random rand;
 	private static int samples;
+	private static int nodesOffset = 0;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int i;
 		File logProperties = new File("log.config");
 		if (!logProperties.exists())
 			BasicConfigurator.configure();
@@ -130,41 +167,155 @@ public class BulkExperiment implements ReportConsumer {
 		}
 
 		try {
-			System.out.println(args[6]);
 			experimentMax = new Integer(args[6]);
-
 		} catch (Exception e) {
 			System.err.println("Invalid number of experiments specified.");
 			printSyntax();
 			return;
 		}
 
+		try {
+			experimentCnt = new Integer(args[7]);
+			System.out.println("starting from experiment " + experimentCnt);
+		} catch (Exception e) {
+			System.out.println("starting from experiment 0");
+			experimentCnt = 0;
+		}
+		try {
+			experimentCnt = new Integer(args[7]);
+			System.out.println("starting from experiment " + experimentCnt);
+		} catch (Exception e) {
+			System.out.println("starting from experiment 0");
+			experimentCnt = 0;
+		}
+		try {
+			nodesOffset = new Integer(args[8]);
+			System.out.println("nodes counting from " + experimentCnt);
+		} catch (Exception e) {
+			System.out.println("nodes counting from 0");
+			nodesOffset = 0;
+		}
+
+		try {
+			sendAlert("Set of experiments started", -1);
+		} catch (Exception e) {
+			System.out.println("Unable to send notification");
+			System.out.println(e.getMessage());
+		}
+
 		gateways = new WSNGateway[nodes];
 		experiment = new BulkExperiment();
 
-		for (int i = 0; i < nodes; i++) {
-			String source = "serial@/dev/ttyUSB" + i + ":tmote";
+		for (i = 0; i < nodes; i++) {
+			String source = "serial@/dev/ttyUSB" + (i + nodesOffset) + ":tmote";
 			gateways[i] = WSNGateway.getGateway(source);
 			gateways[i].registerConsumer(experiment);
 		}
 
+		runNextExperiment();
+	}
+
+	private static void runNextExperiment() {
+		int i = 0;
+
+		long experimentBegin = System.currentTimeMillis();
 		try {
-			for (int i = 0; i < nodes; i++)
+			for (i = 0; i < nodes; i++)
 				gateways[i].startExperiment(period, beacon, samples, duration,
-						rand.nextInt((int) period), countFromMsg, false, true);
-		} catch (IOException e) {
-			System.err.println("Unable to communicate with the mote");
-			System.err.println(e.getMessage());
+						FIXED_DURATION + rand.nextInt((int) period),
+						countFromMsg, false, true);
+		} catch (Exception e) {
+			System.out.println("Experiment " + experimentCnt
+					+ " unable to communicate with mote " + (i + nodesOffset));
+			System.out.println(e.getMessage());
+
+			problemsInRow++;
+			experimentMax++;
+			duration = 60000;
+
+			try {
+				sendAlert(e.getMessage(), i);
+			} catch (Exception e1) {
+				System.out.println("Unable to send notification");
+				System.out.println(e1.getMessage());
+			}
+		}
+
+		if (System.currentTimeMillis() - experimentBegin > FIXED_DURATION) {
+			System.out.println("Experiment " + experimentCnt
+					+ " desynchronized");
+
+			problemsInRow++;
+			experimentMax++;
+			duration = 60000;
+
+			try {
+				sendAlert("Desynchronization error", -1);
+			} catch (Exception e1) {
+				System.out.println("Unable to send notification");
+				System.out.println(e1.getMessage());
+			}
+		}
+
+		if (problemsInRow > ALERT_THRESHOLD) {
+			Toolkit.getDefaultToolkit().beep();
 		}
 
 		Timer timeoutTimer = new Timer();
-		timeoutTimer.schedule(new ExperimentStop(), duration + period * 2);
+		timeoutTimer.schedule(new ExperimentStop(), FIXED_DURATION + duration
+				+ period * 2);
 	}
 
 	private static void printSyntax() {
 		System.out.println("Syntax:");
 		System.out.println("\t java " + BulkExperiment.class.getName()
 				+ " NODES PERIOD BEACON SAMPLES DURATION CNT_MSG EXPERIMENTS");
+	}
+
+	private static void sendAlert(String message, int moteId)
+			throws FileNotFoundException, IOException, AddressException,
+			MessagingException, GeneralSecurityException {
+		Properties props = new Properties();
+
+		props.load(new FileInputStream("mail.config"));
+
+		MailSSLSocketFactory sf = new MailSSLSocketFactory();
+		sf.setTrustAllHosts(true);
+		props.put("mail.smtp.ssl.socketFactory", sf);
+
+		StringBuffer buf = new StringBuffer();
+		buf.append("Host: " + InetAddress.getLocalHost().getHostName() + "\n");
+		buf.append("Experiment: " + experimentCnt + "\n");
+		buf.append("Experiments left: " + (experimentMax - experimentCnt)
+				+ "\n");
+		double eta = (experimentMax - experimentCnt)
+				* (FIXED_DURATION + duration + period * 2) / 1000 / 3600.;
+		buf.append("Estimated time: " + new DecimalFormat("0.##").format(eta)
+				+ "h\n");
+		buf.append("Protocol configuration:\n");
+		buf.append("\tnodes: \t\t" + nodesOffset + " -> "
+				+ (nodes + nodesOffset) + "\n");
+		buf.append("\tperiod: \t\t" + period + "ms\n");
+		buf.append("\tbeacon: \t\t" + beacon + "ms\n");
+		buf.append("\tsamples: \t" + samples + "\n");
+		if (gateways != null && moteId >= 0 && moteId < gateways.length)
+			buf.append("Mote: " + (moteId + nodesOffset) + "\n");
+		buf.append("\n");
+		buf.append(message);
+
+		Session session = Session.getDefaultInstance(props,
+				new SMTPAuthenticator(props));
+		MimeMessage email = new MimeMessage(session);
+
+		Address from[] = new Address[1];
+		from[0] = new InternetAddress("WildMAC experiment <"
+				+ props.getProperty("experiment.from") + ">");
+		email.addFrom(from);
+		email.addRecipient(Message.RecipientType.TO, new InternetAddress(props
+				.getProperty("experiment.to")));
+		email.setSubject("Experiment alert");
+		email.setText(buf.toString());
+		Transport.send(email);
 	}
 
 	private HashMap<Integer, HashMap<Integer, Long>> detections = new HashMap<Integer, HashMap<Integer, Long>>();
